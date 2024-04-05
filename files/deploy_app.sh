4c6570da9b97ec89a90545f966a7d78f3f99d500
adamgilbert912 <adamgilbert912@gmail.com>
2024-04-05 16:44:57 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Buy a cat quick while they're on sale.
   <!-- END -->
 
   </div>
